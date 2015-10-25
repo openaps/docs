3fcb74d5041e7893de29af2008fd526c7a3dfa0b
Scott Leibrand <scottleibrand@gmail.com>
2015-10-25 16:40:36 -0700

    finally working

--- scripts/quick-src.sh
+++ scripts/quick-src.sh
@@ -3,8 +3,7 @@
 sudo apt-get install -y git python python-dev python-setuptools python-software-properties python-numpy python-pip nodejs-legacy npm watchdog && \
 sudo npm install -g json && \
 mkdir src
-cd src && \
-(
+( cd src && \
     git clone -b dev git://github.com/bewest/decoding-carelink.git
     (cd decoding-carelink && \
     sudo python setup.py develop
@@ -20,7 +19,7 @@ cd src && \
     git clone git://github.com/openaps/oref0.git
 )
 test -d src/oref0 && \
-cd src/oref0
+cd src/oref0 && \
 npm install && \
 sudo npm install -g && \
 sudo npm link && \
@@ -30,5 +29,3 @@ sudo openaps-install-udev-rules && \
 sudo activate-global-python-argcomplete && \
 echo openaps installed
 openaps --version
-
-
