4681e78c459b4cbb1b32aa4d4b7a5fa8de9d4759
Scott Leibrand <scottleibrand@gmail.com>
2015-10-25 15:49:55 -0700

    s|git@|git://|g

--- scripts/quick-src.sh
+++ scripts/quick-src.sh
@@ -2,9 +2,9 @@
 
 sudo apt-get install -y git python python-dev python-setuptools python-software-properties python-numpy python-pip nodejs-legacy npm watchdog && \
 (mkdir src; cd src && \
-git clone -b bewest/dev git@github.com:bewest/decoding-carelink.git && \
-git clone git@github.com:bewest/dexcom_reader.git && \
-git clone git@github.com:openaps/openaps.git && \
+git clone -b bewest/dev git://github.com:bewest/decoding-carelink.git && \
+git clone git://github.com:bewest/dexcom_reader.git && \
+git clone git://github.com:openaps/openaps.git && \
 git clone git://github.com/openaps/oref0.git && \
 (cd decoding-carelink && \
 sudo python setup.py develop
