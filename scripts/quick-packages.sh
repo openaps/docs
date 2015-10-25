17aaa98c8d6481c53647532a23f0f51b15514824
Scott Leibrand <scottleibrand@gmail.com>
2015-10-25 15:43:09 -0700

    watchdog

--- scripts/quick-packages.sh
+++ scripts/quick-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install -y git python python-dev python-setuptools python-software-properties python-numpy python-pip nodejs-legacy npm && \
+sudo apt-get install -y git python python-dev python-setuptools python-software-properties python-numpy python-pip nodejs-legacy npm watchdog && \
 sudo easy_install -ZU setuptools && \
 sudo easy_install -ZU openaps && \
 sudo openaps-install-udev-rules && \
