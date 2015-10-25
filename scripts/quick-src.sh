6a04d624ac91489beaca2e181ab32f7258e0ce0c
Scott Leibrand <scottleibrand@gmail.com>
2015-10-25 15:45:33 -0700

    syntax

--- scripts/quick-src.sh
+++ scripts/quick-src.sh
@@ -7,21 +7,21 @@ git clone git@github.com:bewest/dexcom_reader.git && \
 git clone git@github.com:openaps/openaps.git && \
 git clone git://github.com/openaps/oref0.git && \
 (cd decoding-carelink && \
-sudo python setup.py develop && \
+sudo python setup.py develop
 ) && \
 (cd dexcom_reader && \
-sudo python setup.py develop && \
+sudo python setup.py develop
 ) && \
 (cd openaps && \
-sudo python setup.py develop && \
-) && \
+sudo python setup.py develop
+)
 ) && \
 test -d src/ && sudo chown -R $USER src/
 test -d src/oref0 && (cd src/oref0
 npm install && \
 sudo npm install -g && \
 sudo npm link && \
-sudo npm link oref0 && \
+sudo npm link oref0
 ) && \
 sudo openaps-install-udev-rules && \
 sudo activate-global-python-argcomplete && \
