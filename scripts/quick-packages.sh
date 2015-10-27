a091c5d5bb311a8f1adf8e4154eee9031c8a99e4
Scott Leibrand <scottleibrand@gmail.com>
2015-10-26 18:00:52 -0700

    replace python-setuptools with https://bootstrap.pypa.io/ez_setup.py per @bewest

--- scripts/quick-packages.sh
+++ scripts/quick-packages.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install -y git python python-dev python-setuptools python-software-properties python-numpy python-pip nodejs-legacy npm watchdog && \
+sudo apt-get install -y git python python-dev python-software-properties python-numpy python-pip nodejs-legacy npm watchdog && \
+curl -s https://bootstrap.pypa.io/ez_setup.py | sudo python && \
 sudo easy_install -ZU setuptools && \
 sudo easy_install -ZU openaps && \
 sudo openaps-install-udev-rules && \
