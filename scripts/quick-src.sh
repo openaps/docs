5cd673090b71beba2e1dd9ed0bfe8da3e1bf8df6
Scott Leibrand <scottleibrand@gmail.com>
2015-11-17 22:42:55 -0800

    attempt git pull if directory already exists

--- scripts/quick-src.sh
+++ scripts/quick-src.sh
@@ -4,26 +4,31 @@ sudo apt-get install -y git python python-dev python-software-properties python-
 ( curl -s https://bootstrap.pypa.io/ez_setup.py | sudo python ) && \
 sudo npm install -g json && \
 sudo easy_install -ZU setuptools && \
-mkdir src
+cd && mkdir src
 cd src && \
 (
-    git clone -b dev git://github.com/bewest/decoding-carelink.git
+    git clone -b dev git://github.com/bewest/decoding-carelink.git || \
+        (cd decoding-carelink && git pull)
     (cd decoding-carelink && \
-    sudo python setup.py develop
+        sudo python setup.py develop
     )
-    git clone git://github.com/bewest/dexcom_reader.git
+    git clone git://github.com/bewest/dexcom_reader.git || \
+        (cd dexcom_reader && git pull)
     (cd dexcom_reader && \
-    sudo python setup.py develop
+        sudo python setup.py develop
     )
-    git clone -b dev git://github.com/openaps/openaps.git
+    git clone -b dev git://github.com/openaps/openaps.git || \
+        (cd openaps && git pull)
     (cd openaps && \
-    sudo python setup.py develop
+        sudo python setup.py develop
     )
-    git clone -b dev git://github.com/openaps/openaps-contrib.git
+    git clone -b dev git://github.com/openaps/openaps-contrib.git || \
+        (cd openaps-contrib && git pull)
     (cd openaps-contrib && \
-    sudo python setup.py develop
+        sudo python setup.py develop
     )
-    git clone -b dev git://github.com/openaps/oref0.git
+    git clone -b dev git://github.com/openaps/oref0.git || \
+        (cd openaps-contrib && git pull)
 )
 test -d oref0 && \
 cd oref0 && \
