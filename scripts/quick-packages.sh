5dc4d1f44d0ceb552b6c970b4374322d2a6e3fc3
Scott Leibrand <scottleibrand@gmail.com>
2016-09-18 23:56:47 -0700

    Switch to using pip
    Switch to using pip for openaps and openaps-contrib per https://github.com/openaps/docs/issues/133

--- scripts/quick-packages.sh
+++ scripts/quick-packages.sh
@@ -2,10 +2,12 @@
 
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y git python python-dev python-software-properties python-numpy python-pip nodejs-legacy npm watchdog && \
-( curl -s https://bootstrap.pypa.io/ez_setup.py | sudo python ) && \
-sudo easy_install -ZU setuptools && \
-sudo easy_install -ZU openaps && \
-sudo easy_install -ZU openaps-contrib && \
+# ( curl -s https://bootstrap.pypa.io/ez_setup.py | sudo python ) && \
+# sudo easy_install -ZU setuptools && \
+# sudo easy_install -ZU openaps && \
+# sudo easy_install -ZU openaps-contrib && \
+pip install -U openaps && \
+pip install -U openaps-contrib && \
 sudo openaps-install-udev-rules && \
 sudo activate-global-python-argcomplete && \
 sudo npm install -g json oref0 && \
