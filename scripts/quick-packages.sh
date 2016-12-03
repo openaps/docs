28336e5c5b877c2ee8f2de8719ca452c1001885d
Scott Leibrand <scottleibrand@gmail.com>
2016-12-02 21:06:15 -0800

    remove old commented code

--- scripts/quick-packages.sh
+++ scripts/quick-packages.sh
@@ -2,10 +2,6 @@
 
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y git python python-dev python-software-properties python-numpy python-pip nodejs-legacy npm watchdog && \
-# ( curl -s https://bootstrap.pypa.io/ez_setup.py | sudo python ) && \
-# sudo easy_install -ZU setuptools && \
-# sudo easy_install -ZU openaps && \
-# sudo easy_install -ZU openaps-contrib && \
 sudo pip install -U openaps && \
 sudo pip install -U openaps-contrib && \
 sudo openaps-install-udev-rules && \
