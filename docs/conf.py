7f02740583b34376cce581ae6ae6fc2e2b8557e0
Dana Lewis <danamlewis@users.noreply.github.com>
2017-03-22 15:07:25 -0700

    Attempting to tweak RTD theme in dev

--- docs/conf.py
+++ docs/conf.py
@@ -122,7 +122,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'default`
 
 
 extra_nav_links = {
