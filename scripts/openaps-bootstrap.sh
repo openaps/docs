12ad01dcc6959c8a13623998ccc8a794b77dac44
Scott Leibrand <scottleibrand@gmail.com>
2017-05-20 22:03:42 -0700

    rudimentary wifi bootstrapping script for openaps

---
+++ scripts/openaps-bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+echo Scanning for wifi networks:
+wpa_cli scan
+echo -e "\nStrongest networks found:"
+wpa_cli scan_res | sort -grk 3 | head | awk -F '\t' '{print $NF}' | uniq
+read -p "Enter your network name: " -r
+SSID=$REPLY
+read -p "Enter your network password: " -r
+PSK=$REPLY
+cd /etc/network
+cp interfaces interfaces.$(date +%s).bak
+echo -e "auto lo\niface lo inet loopback\n\nauto usb0\niface usb0 inet static\n  address 10.11.12.13\n  netmask 255.255.255.0\n\nauto wlan0\niface wlan0 inet dhcp\n  wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf" > interfaces
+echo -e "\n/etc/network/interfaces:\n"
+cat interfaces
+cd /etc/wpa_supplicant/
+cp wpa_supplicant.conf wpa_supplicant.conf.$(date +%s).bak
+echo -e "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\nnetwork={\n  ssid=\"$SSID\"\n  psk=\"$PSK\"\n}" > wpa_supplicant.conf
+echo -e "\n/etc/wpa_supplicant/wpa_supplicant.conf:\n"
+cat wpa_supplicant.conf
+echo -e "\nAttempting to bring up wlan0:\n"
+ifdown wlan0; ifup wlan0
+echo -n "Wifi SSID: "; iwgetid -r
+curl -s https://raw.githubusercontent.com/openaps/docs/dev/scripts/openaps-install.sh | bash -
