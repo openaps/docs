c02eadfb72c626a730dd23175270b2ffe9e78e1f
Scott Leibrand <scottleibrand@gmail.com>
2017-07-07 14:04:06 -0700

    pause before running oref0-setup

--- scripts/openaps-install.sh
+++ scripts/openaps-install.sh
@@ -25,4 +25,6 @@ sed -i "s/#compress/compress/g" /etc/logrotate.conf
 
 curl -s https://raw.githubusercontent.com/openaps/docs/master/scripts/quick-packages.sh | bash -
 mkdir -p ~/src; cd ~/src && git clone git://github.com/openaps/oref0.git || (cd oref0 && git checkout master && git pull)
+echo "Press Enter to run oref0-setup with the current release (master branch) of oref0,"
+read -p "or press ctrl-c to cancel. " -r
 cd && ~/src/oref0/bin/oref0-setup.sh
