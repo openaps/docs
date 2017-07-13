986394653a9242a8f4865dfe8389d0ab7d4bf8de
Scott Leibrand <scottleibrand@gmail.com>
2017-07-12 22:08:53 -0700

    install nodejs 6.x instead of the old 0.10 that debian defaults to

--- scripts/quick-packages.sh
+++ scripts/quick-packages.sh
@@ -3,7 +3,9 @@
 # TODO: remove the `-o Acquire::ForceIPv4=true` once Debian's mirrors work reliably over IPv6
 apt-get -o Acquire::ForceIPv4=true install -y sudo
 sudo apt-get -o Acquire::ForceIPv4=true update && sudo apt-get -o Acquire::ForceIPv4=true -y upgrade
-sudo apt-get -o Acquire::ForceIPv4=true install -y git python python-dev python-software-properties python-numpy python-pip nodejs-legacy npm watchdog strace tcpdump screen acpid vim locate jq lm-sensors && \
+# install nodejs 6.x instead of the old 0.10 that debian defaults to
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get -o Acquire::ForceIPv4=true install -y git python python-dev python-software-properties python-numpy python-pip nodejs npm watchdog strace tcpdump screen acpid vim locate jq lm-sensors && \
 sudo pip install -U openaps && \
 sudo pip install -U openaps-contrib && \
 sudo openaps-install-udev-rules && \
