bfc09f323c3a058a51b51bc3e54399965b4a51eb
Scott Leibrand <scottleibrand@gmail.com>
2017-07-12 22:36:06 -0700

    install mosh

--- scripts/quick-packages.sh
+++ scripts/quick-packages.sh
@@ -5,7 +5,7 @@ apt-get -o Acquire::ForceIPv4=true install -y sudo
 sudo apt-get -o Acquire::ForceIPv4=true update && sudo apt-get -o Acquire::ForceIPv4=true -y upgrade
 # install nodejs 6.x instead of the old 0.10 that debian defaults to
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get -o Acquire::ForceIPv4=true install -y git python python-dev python-software-properties python-numpy python-pip nodejs npm watchdog strace tcpdump screen acpid vim locate jq lm-sensors && \
+sudo apt-get -o Acquire::ForceIPv4=true install -y git python python-dev python-software-properties python-numpy python-pip nodejs npm watchdog strace tcpdump screen acpid vim locate jq lm-sensors mosh && \
 sudo pip install -U openaps && \
 sudo pip install -U openaps-contrib && \
 sudo openaps-install-udev-rules && \
