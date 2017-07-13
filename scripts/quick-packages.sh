1046b01afb6189afe9f41a3b858cd7e0fa89c816
Scott Leibrand <scottleibrand@gmail.com>
2017-07-12 22:42:52 -0700

    let's do this systemwide instead

--- scripts/quick-packages.sh
+++ scripts/quick-packages.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# TODO: remove the `-o Acquire::ForceIPv4=true` once Debian's mirrors work reliably over IPv6
-apt-get -o Acquire::ForceIPv4=true install -y sudo
-sudo apt-get -o Acquire::ForceIPv4=true update && sudo apt-get -o Acquire::ForceIPv4=true -y upgrade
+apt-get install -y sudo
+sudo apt-get update && sudo apt-get -y upgrade
 # install nodejs 6.x instead of the old 0.10 that debian defaults to
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get -o Acquire::ForceIPv4=true install -y git python python-dev python-software-properties python-numpy python-pip nodejs npm watchdog strace tcpdump screen acpid vim locate jq lm-sensors mosh && \
+sudo apt-get install -y git python python-dev python-software-properties python-numpy python-pip nodejs npm watchdog strace tcpdump screen acpid vim locate jq lm-sensors mosh && \
 sudo pip install -U openaps && \
 sudo pip install -U openaps-contrib && \
 sudo openaps-install-udev-rules && \
