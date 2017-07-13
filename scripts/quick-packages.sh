c25ebe27ee94078eb55644d6f4e2d9e5d7975094
Scott Leibrand <scottleibrand@gmail.com>
2017-07-12 23:03:41 -0700

    try to resolve 'nodejs : Conflicts: npm'

--- scripts/quick-packages.sh
+++ scripts/quick-packages.sh
@@ -4,7 +4,7 @@ apt-get install -y sudo
 sudo apt-get update && sudo apt-get -y upgrade
 # install nodejs 6.x instead of the old 0.10 that debian defaults to
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y git python python-dev python-software-properties python-numpy python-pip nodejs npm watchdog strace tcpdump screen acpid vim locate jq lm-sensors mosh && \
+sudo apt-get install -y git python python-dev python-software-properties python-numpy python-pip nodejs watchdog strace tcpdump screen acpid vim locate jq lm-sensors mosh && \
 sudo pip install -U openaps && \
 sudo pip install -U openaps-contrib && \
 sudo openaps-install-udev-rules && \
