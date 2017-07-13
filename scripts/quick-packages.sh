8e0eb361a54826d8301d5ce5893d259d2aa99845
Scott Leibrand <scottleibrand@gmail.com>
2017-07-12 21:13:02 -0700

    force IPv4 for now to make apt-get run at a reasonable speed

--- scripts/quick-packages.sh
+++ scripts/quick-packages.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-apt-get install -y sudo
-sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install -y git python python-dev python-software-properties python-numpy python-pip nodejs-legacy npm watchdog strace tcpdump screen acpid vim locate jq lm-sensors && \
+# TODO: remove the `-o Acquire::ForceIPv4=true` once Debian's mirrors work reliably over IPv6
+apt-get -o Acquire::ForceIPv4=true install -y sudo
+sudo apt-get -o Acquire::ForceIPv4=true update && sudo apt-get -o Acquire::ForceIPv4=true -y upgrade
+sudo apt-get -o Acquire::ForceIPv4=true install -y git python python-dev python-software-properties python-numpy python-pip nodejs-legacy npm watchdog strace tcpdump screen acpid vim locate jq lm-sensors && \
 sudo pip install -U openaps && \
 sudo pip install -U openaps-contrib && \
 sudo openaps-install-udev-rules && \
