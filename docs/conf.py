41794f9abf3b562608a07916512ccef4d864b9d5
Kim Scott <kimscott@mit.edu>
2019-09-11 10:41:10 -0400

    empirically checking rtd url_resolver default sorry

--- docs/conf.py
+++ docs/conf.py
@@ -419,7 +419,7 @@ if on_rtd:
 def setup(app):
     app.add_config_value('recommonmark_config', {
             # 'url_resolver': lambda url: github_doc_root + url,
-            'url_resolver': lambda url: hosted_root + url + '.html',
+            # 'url_resolver': lambda url: hosted_root + url + '.html',
             'auto_toc_tree_section': 'Summary',
             'enable_auto_doc_ref': True,
             'enable_eval_rst': True,
